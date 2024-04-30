f6c5a0068fd86de0a73eb5a6d59d18ea8144a1f2
veselovvv <vycheslav5veselov@gmail.com>
2024-04-30 19:21:13 +0300

    feature/Movies2.0_001_movies Fixed date mapping in BaseMovieDomainToUiMapper

--- app/src/main/java/com/veselovvv/movies20/movies/presentation/BaseMovieDomainToUiMapper.kt
+++ app/src/main/java/com/veselovvv/movies20/movies/presentation/BaseMovieDomainToUiMapper.kt
@@ -3,6 +3,8 @@ package com.veselovvv.movies20.movies.presentation
 import com.veselovvv.movies20.movies.domain.MovieDomainToUiMapper
 
 class BaseMovieDomainToUiMapper : MovieDomainToUiMapper {
-    override fun map(id: Int, posterPath: String, releaseDate: String, title: String) =
-        MovieUi.Base(id, posterPath, releaseDate, title)
+    override fun map(id: Int, posterPath: String, releaseDate: String, title: String): MovieUi {
+        val releaseYear = releaseDate.substring(0, 4)
+        return MovieUi.Base(id, posterPath, releaseYear, title)
+    }
 }
